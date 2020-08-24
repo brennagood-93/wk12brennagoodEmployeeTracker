ae5e4c6be35ecf8e97e8864e9fe470c63b75fc01
Brenna Good <brenna.elise.good@gmail.com>
2020-08-23 19:00:51 -0700

    created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+create database emptracker;
+use emptracker;
+create table department(
+    id int auto_increment,
+    name varchar,
+    primary key (id)
+);
+create table roles(
+    id int auto_increment,
+    title varchar,
+    salary int,
+    department int,
+    primary key (id)
+);
+create table employee(
+    id int auto_increment,
+    first_name varchar(20),
+    last_name varchar(25),
+    role_id int,
+    manager_id int,
+    primary key (id)
+);
